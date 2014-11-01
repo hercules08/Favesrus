403314bceb480e3e9087ee23b48fd228a047dcb7
eashtianjr <elroy.ashtian@gmail.com>
2014-11-01 12:50:16 -0700

    upload project files

---
+++ www/js/main.js
@@ -0,0 +1,17 @@
+/*jslint vars:true, plusplus:true */
+/*global window, $ */
+
+//counter for numbering the Person an Details input fields
+var counter = 1;
+
+//Disable the animation of the header name
+$.ui.animateHeaders = false;
+
+//Disable native look and feel
+$.ui.useOSThemes = false;
+
+/*
+Function: functionName()
+Parameter: parameterName is the 
+Description:
+*/
