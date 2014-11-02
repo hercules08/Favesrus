ff99914f27ee27e04b0e8b67e64bd0d3050718ad
aomotosh <adedamola.omotosho@intel.com>
2014-11-01 17:13:27 -0700

    Add server project

---
+++ server/Project-G/Project-G.Server/BLL/BusinessObjects/RetailerBO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_G.Server.BLL.BusinessObjects
+{
+    public class RetailerBO
+    {
+        public int RetailerId { get; set; }
+        public string RetailerName { get; set; }
+        public string RetailerLogo { get; set; }
+        public string RetailerLogoDataString { get; set; }
+    }
+}
---
+++ server/Project-G/Project-G.Server/BLL/Repos/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_G.Server.BLL.Repos
+{
+    public class UserRepository
+    {
+    }
+}
