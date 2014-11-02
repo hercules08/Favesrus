c90cca10017fb9ac21858365ccd29fb2be1221d7
eashtianjr <elroy.ashtian@gmail.com>
2014-11-01 19:33:12 -0700

    update project files for favs_details & selections page

--- mobile/www/js/main.js
+++ mobile/www/js/main.js
@@ -13,13 +13,41 @@ $.ui.useOSThemes = false;
 
 window.onload = function () {
 	//Set the size of div to a square (50% of the device width)
-	$(".temp").width(0.5 * window.innerWidth);
-	$(".temp").height(0.5 * window.innerHeight);
+	$(".red_bg").width(0.5 * window.innerWidth);
+	$(".red_bg").height(0.5 * window.innerHeight);
 
-	//apply an onclick event handler
+	//apply an onclick event handler to the play! button
 	$("#play").click(function () {
 		$.ui.loadContent("#forth", true, true, "fade");
 	});
+
+	//Stretch the Favorites Row accross two rows 
+	$("#temp2").attr("colspan", "'2'");
+
+	//Set the div background for the Favs section to the size of table
+	/*$("#temp2").width(0.8 * window.innerWidth);
+	$("#temp2").height(0.25 * window.innerHeight);*/
+
+	//Set the size of selction A & B div for this of This OR That! 
+	$("#selectionA").width(0.5 * window.innerWidth);
+	$("#selectionB").width(0.5 * window.innerWidth);
+
+	//apply an onclick event handler to the Favs div
+	$("#favs_row").click(function () {
+		$.ui.loadContent("#favs_details_page", true, true, "fade");
+	});
+
+	//Set the size of ask_network div for for the Favs Details Page 
+	$(".suggestion_item").width(0.25 * window.innerWidth);
+	$(".suggestion_item").height(0.15 * window.innerHeight);
+
+
+	//Set the size of ask_network button div for for the Favs Details Page 
+	$("#ask_network_button").width(0.2 * window.innerWidth);
+	$("#ask_network_button").height(0.2 * window.innerHeight);
+
+	//Set the size of ask_network button div for for the Favs Details Page 
+	$("#ask_network_column").width(0.25 * window.innerWidth);
 }
 
 /*
