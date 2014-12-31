04cae96b9eeed1858513548fee45305383979901
Damola Omotosho <damola.omotosho@gmail.com>
2014-12-31 03:57:00 -0800

    Update client and server side User models

--- server/Favit/Favit.Model/Entities/User.cs
+++ server/Favit/Favit.Model/Entities/User.cs
@@ -8,12 +8,16 @@ namespace Favit.Model.Entities
     {
         public virtual string FirstName { get; set; }
         public virtual string LastName { get; set; }
+        public virtual string UserName { get; set; }
         public virtual string Email { get; set; }
-        public virtual string ModoId { get; set; }
+        public virtual string Password { get; set; }
+        public virtual Nullable<System.DateTime> Birthday { get; set; }
         public virtual string PhoneNumber { get; set; }
         public virtual string Gender { get; set; }
-        public virtual Nullable<System.DateTime> Birthday { get; set; }
-        public virtual ICollection<User> Favs { get; set; }
+        public virtual string ModoId { get; set; }
+        public virtual Nullable<DateTime> ModoMembershipDate { get; set; }
+        public virtual bool CardOnFile { get; set; }
+        public virtual ICollection<User> FavFriends { get; set; }
         public virtual ICollection<Item> FavItems { get; set; }
 
         public string Pic { get; set; }
--- server/Favit/Favit.Web.Api.Models/User.cs
+++ server/Favit/Favit.Web.Api.Models/User.cs
@@ -11,13 +11,17 @@ namespace Favit.Web.Api.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
         public string Email { get; set; }
-        public string ModoId { get; set; }
+        public DateTime Birthday { get; set; }
         public string PhoneNumber { get; set; }
         public string Gender { get; set; }
-        public Nullable<System.DateTime> Birthday { get; set; }
-        public ICollection<User> Favs { get; set; }
-        public ICollection<Item> FavItems { get; set; }
+        public string ModoId { get; set; }
+        public DateTime ModoMembershipDate { get; set; }
+        public bool CardOnFile { get; set; }
+        public ICollection<User> FavFriends { get; set; }
+        public ICollection<Item> FavItems { get; set; } 
 
         public string Pic { get; set; }
     }
