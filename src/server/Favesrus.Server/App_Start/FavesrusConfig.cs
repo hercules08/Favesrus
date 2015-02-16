208fa8f63319c0d965b1afb09ff261d32931e1bc
Damola Omotosho <damola.omotosho@gmail.com>
2015-02-16 15:35:04 -0800

    Add Base Controller for styling

---
+++ src/server/Favesrus.Server/App_Start/FavesrusConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(Favesrus.Server.FavesrusConfig))]
+
+namespace Favesrus.Server
+{
+    public class FavesrusConfig
+    {
+        public void Configuration(IAppBuilder app) { }
+    }
+}
