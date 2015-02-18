5da6a84cd9a6aff10230b82a6b0be2c706864a61
Damola Omotosho <damola.omotosho@gmail.com>
2015-02-18 00:49:09 -0800

    Begin adding Controllers for facebook login & local account login

---
+++ src/server/Favesrus.Server/Controllers/WebApi/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Favesrus.Server.Controllers.WebApi
+{
+    public class AccountController : ApiController
+    {
+    }
+}
