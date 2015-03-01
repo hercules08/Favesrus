21a4243186c3301f4790d5285d8b5ea4a8817979
Damola Omotosho <damola.omotosho@gmail.com>
2015-02-28 16:23:26 -0800

    Update Identity EF to take max string length for provider key

--- src/server/Favesrus.DAL/Impl/FavesrusDbContext.cs
+++ src/server/Favesrus.DAL/Impl/FavesrusDbContext.cs
@@ -29,6 +29,9 @@ namespace Favesrus.DAL.Impl
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Microsoft.AspNet.Identity.EntityFramework.IdentityUserLogin>().ToTable("AspNetUserLogins")
+                .Property(c => c.ProviderKey).HasMaxLength(Int32.MaxValue);
         }
 
     }
